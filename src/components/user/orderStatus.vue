84a2c5ae958623c5e28185654ce98aed290dd90f
957685825 <957685825@qq.com>
2017-06-30 16:40:18 +0800

    sys

--- src/components/user/orderStatus.vue
+++ src/components/user/orderStatus.vue
@@ -29,11 +29,28 @@
         	Api.car.orderPay(jsons).then(res=>{
         		if(res.data.code === 'success'){
 						if(this.$route.query.paymentType==='WX'){//手机微信支付
-							wxpay(d,callback); 
+							wxpay(res.data,function callback(res){
+								if(res.errMsg=="chooseWXPay:fail"){
+									
+									//updateOrderStatus(orderDbIds);
+									
+									alert("调起支付失败");
+									//window.location.href = url;
+									
+								}else if(res.errMsg == "chooseWXPay:cancel" ) {//用户取消订单
+								
+									//updateOrderStatus(orderDbIds);
+								
+									alert("调起支付失败");			
+								}else{
+									alert("支付成功");
+									//window.location.href = url;
+								}
+							}); 
 						}
 					} else {
 						Toast('支付失败');
-        				location.href="#orderList"
+        				//location.href="#orderList"
 						
 					}
         	},err=>{
