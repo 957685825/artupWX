fef23221a9732744f4b3bea42a346b57deb1d394
957685825 <957685825@qq.com>
2017-07-04 17:09:03 +0800

    s

--- src/components/index/Index.vue
+++ src/components/index/Index.vue
@@ -13,12 +13,43 @@
 		</div>
 		<div v-DomHeight class="btn_btn">
 			<ul class="btn_ul">
-				<li> <router-link to="bbsMsg?category=baobaoshu">宝宝书</router-link></li>
-				<li> <router-link to="">画册</router-link></li>
-				<li> <router-link to="">台历</router-link></li>
-				<li> <router-link to="">框画</router-link></li>
-				<li> <router-link to="">其他产品</router-link></li>				
-				<li> <router-link to="user">个人中心</router-link></li>
+				<li>
+					
+					<router-link to="bbsMsg?category=baobaoshu">
+						 <i style="font-size: 3.1875rem;" class="icon iconfont">&#xe625;</i>
+				 		<p>宝宝书</p>
+					</router-link>
+				</li>
+				<li>
+					<router-link to="">
+						<i style="font-size: 3.1875rem;" class="icon iconfont">&#xe7ca;</i>
+						<p>画册</p>
+					</router-link>
+				</li>
+				<li> 
+					<router-link to="">
+						<i style="font-size: 3.1875rem;" class="icon iconfont">&#xe686;</i>
+						<p>台历</p>
+					</router-link>
+				</li>
+				<li>
+					<router-link to="">
+						<i style="font-size: 3.1875rem;" class="icon iconfont">&#xe628;</i>
+						<p>框画</p>
+					</router-link>
+				</li>
+				<li> 
+					<router-link to="">
+						<i style="font-size: 3.1875rem;" class="icon iconfont">&#xe609;</i>
+						<p>其他产品</p>
+					</router-link>
+				</li>				
+				<li> 
+					<router-link to="user">
+						<i style="font-size: 3.1875rem;" class="icon iconfont">&#xe64b;</i>
+						<p>个人中心</p>
+					</router-link>
+				</li>
 			</ul>
 		</div>
 	</div>
