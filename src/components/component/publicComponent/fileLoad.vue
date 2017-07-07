f59048e38a0e92e8c2a16d7f02a6caa9bb3907d3
pengjin0 <827879040@qq.com>
2017-07-07 09:16:36 +0800

    同步

---
+++ src/components/component/publicComponent/fileLoad.vue
@@ -0,0 +1,46 @@
+<template>
+	<div id="fileLoad">
+		<!-- 弹出框选上传方式 -->
+		<mt-popup class="fileUpload" style="position;relative;"  v-model="sheetVisible"  closeOnClickModal="false" position="bottom">
+			<ul>
+				<li id="browseButton">从本地添加</li>
+				<li v-tap="{methods : slectUpload}">从素材库里导入</li>
+				<li @click="sheetVisible=false">取消</li>
+			</ul>
+		</mt-popup>
+		<!-- 弹出框图片素材库 -->
+		<mt-popup style="position;relative;"
+				  v-model="popupVisible"
+				  position="bottom">
+			<div class="img_div">
+				<!--liActive-->
+				<ul>
+					<li  v-tap="{methods : MaterialCheckImg ,index : index}"  :class="imgs.activeLi ? 'liActive ':'' " v-for="(imgs,index) in bbs.Material" >
+						<img :src="imgs.thumbnailUrl" >
+					</li>
+				</ul>
+				<div class="btn_button">
+					<a class="Determine" v-tap="{methods : okQuery}">确认</a>
+				</div>
+			</div>
+		</mt-popup>
+	</div>	
+</template>
+
+<script>
+	import { Toast ,Actionsheet,Popup,Indicator} from 'mint-ui';
+    import Api from '../../../API.js'
+    export default{ 
+    		   
+     	 data () {
+            return {
+            	
+         }}
+     	 
+     	 
+     }
+	
+</script>
+
+<style>
+</style>
