27930ed8adcce7dd264103e12f407dd817bd71fd
957685825 <957685825@qq.com>
2017-07-25 10:19:40 +0800

    s

--- static/lab/js/cropit-builder.js
+++ static/lab/js/cropit-builder.js
@@ -3,41 +3,52 @@
 					//var imageData = $('.image-editor').cropit('export');
 					//选择图片
 					var myImg = document.querySelector(".cropit-preview-image");
-					var matrix = window.getComputedStyle(myImg).transform;
+					//var matrix = window.getComputedStyle(myImg).transform;
+					var matrix = $(myImg).css('transform');
 					//框的宽度
 					var kuangWidth = $('.cropit-preview-image-container').width();
 					var kuangHeight = $('.cropit-preview-image-container').height();
 					//var aa = $('.image-editor').cropit('rotates', myImg.action);
-					var angule = document.querySelector(".cropit-preview-image").style.transform;
-					var arr = matrix.replace(/[^0-9\-\.,]/g, '').split(',')
-					//X轴方向的偏移量
-					var deviationX = Number(arr[4]);
-					//Y轴偏移量
-					var deviationY = Number(arr[5]);
+					//var angule = document.querySelector(".cropit-preview-image").style.transform;
+					var styles = $(".cropit-preview-image").attr("style");
+					var arr = matrix.replace(/[^0-9\-\.,]/g, '').split(',');
+					var angule;//获取到的transfrom里面的属性
+					var anguleArr = styles.split(';');
+					for(var i =0; i<anguleArr.length; i++){
+						if(anguleArr[i].indexOf('transform:') > -1){
+							var transform = anguleArr[i].substring(12);
+							if(transform.indexOf('translate') > -1){
+								angule = transform;
+
+							}
+							
+						}
+					}
+					
 					//定义要判断的字符串
-					var str = angule;
+					//var str = angule;
 					//定义正则表达式对象，\表示转义字符,原点表示任意字符，+表示出现次数至少1次，igm表示忽略大小写，且全局匹配
 					pattern = new RegExp("\\((.| )+?\\)", "igm");
 					//取出匹配正则表达式的内容
-					var newStr = str.match(pattern);
+					var newStr = angule.match(pattern);
+					
 					//缩放比例
 					var scale = Number(newStr[1].replace(/\(/,"").replace(/\)/,""))
 				    //旋转角度
 				    var rotat = Number(newStr[2].replace(/\(/,"").replace(/\)/,"").replace("deg",""));
 				    //判断偏移量第一次的没有偏移的时候如果不是number是NaN的时候手动赋值0
+				    var deviation = newStr[0].replace(/\(/,"").replace(/\)/,"").split(',');
+
+					//X轴方向的偏移量
+					var deviationX = Number(deviation[0].replace('px',''));
+					//Y轴偏移量
+					var deviationY = Number(deviation[1].replace('px',''));
 
 				    if(isNaN(deviationX)){
 				       deviationX = 0;
 				    }if(isNaN(deviationY)){
 				       deviationY = 0;
 				    }
-
-				    if(deviationX == kuangWidth){
-				       //deviationX = 0;
-				    }if(deviationY == kuangHeight){
-				       //deviationY = 0;
-				    }
-
 			        //alert(kuangWidth/scale*120)
 			        //原图中的偏移量X
 			        var nowdeviationX = Number(Math.abs(deviationX)/scale).toFixed(2);
@@ -72,10 +83,9 @@
 			        cropitData.height = nwHeight;
 			        cropitData.rotate = rotat;
 			        cropitData.scale = scale;
+
 			        return cropitData;
-					//var dataStr = '{\"x\":' + nowdeviationX + ', \"y\":' + nowdeviationY + ', \"width\":' + nowWidth + ', \"height\":' + nwHeight + ', \"rotate\":' + rotat;
-					//console.log(dataStr);
-					//return dataStr;
+					
 		
 		
 		}
